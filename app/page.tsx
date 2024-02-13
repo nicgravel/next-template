94375bc3434a8c7382658343d6104ccb6c2131b9
nicgravel <nicgravel@gmail.com>
2024-02-13 14:57:39 -0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <section className="py-24">
+      <div className="container mx-auto px-4">
+        <h1 className="text-5xl font-bold text-center text-gray-800">
+          Welcome to Next Template
+        </h1>
+      </div>
+    </section>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        md: "1.5rem",
+        lg: "2rem",
+      },
+    },
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
